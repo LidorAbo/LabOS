6c51136e3e805598341725adbc2040f16072f5e1
LidorAbo <lidorabo2@gmail.com>
2022-07-06 22:53:27 +0300

    Reforamt python file

--- docker/app.py
+++ docker/app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 import requests
+
 app = Flask(__name__)
+
+
 @app.route('/<owner>/<repo_name>/')
 def welcome(owner, repo_name):
     response = requests.get(f"https://api.github.com/repos/{owner}/{repo_name}/releases/latest")
     return response.json()["name"]
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
